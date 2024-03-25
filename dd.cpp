aaa084153a42ea898417d11785079f54e0733539
Hanjinwoo <chococan16@gmail.com>
2024-03-26 00:31:40 +0900

    24_03_26

--- dd.cpp
+++ dd.cpp
@@ -1,60 +1,54 @@
 #include <algorithm>
+#include <cstring>
 #include <iostream>
+#include <queue>
 #include <vector>
-
 using namespace std;
-int N;
-char arr[30][30];
-int visited[30][30];
-int cnt;
-vector<int> num_cnt;
 
+int n, m;
+char arr[101][101];
 int dr[] = {-1, 0, 1, 0};
 int dc[] = {0, 1, 0, -1};
-
-void dfs(int row, int col) {
-  for (int i = 0; i < 4; i++) {
-    int nr = row + dr[i];
-    int nc = col + dc[i];
-    if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
-      // 맵바깥으로 넘어가는 경우 예외처리
-      continue;
-    }
-    if (arr[nr][nc] == '0') {
-      continue;
-    }
-    if (visited[nr][nc] == 1) {
-      continue;
-    }
-    if (arr[nr][nc] == '1') {
-      visited[nr][nc] = 1;
-      cnt++;
-      dfs(nr, nc);
-    }
-  }
-}
+int visit[101][101];
+int cnt = 1;
 int main() {
-  cin >> N;
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
       cin >> arr[i][j];
     }
   }
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
-      if (arr[i][j] == '1' && visited[i][j] != 1) {
-        visited[i][j] = 1;
-        cnt++;
-        dfs(i, j);
-        num_cnt.push_back(cnt);
-        cnt = 0;
+
+  int row = 0;
+  int col = 0;
+  visit[row][col] = 1;
+  queue<pair<int, int>> q;
+  q.push(make_pair(row, col));
+  while (!q.empty()) {
+    row = q.front().first;
+    col = q.front().second;
+    q.pop();
+    for (int i = 0; i < 4; i++) {
+      int nr = row + dr[i];
+      int nc = col + dc[i];
+      if (nr < 0 || nc < 0 || nr >= n || nc >= m) {
+        continue;
+      }
+      if (visit[nr][nc] != 0) {
+        continue;
+      }
+      if (arr[nr][nc] == '0') {
+        continue;
+      }
+      if (arr[nr][nc] == '1') {
+        q.push(make_pair(nr, nc));
+        visit[nr][nc] = visit[row][col] + 1;
+      }
+      if (nr == n - 1 && nc == m - 1) {
+        cout << visit[nr][nc];
+        // return 0;
       }
     }
-  }
-  cout << num_cnt.size() << endl;
-  sort(num_cnt.begin(), num_cnt.end());
-  for (int i = 0; i < num_cnt.size(); i++) {
-    cout << num_cnt[i] << endl;
   }
 
   return 0;
