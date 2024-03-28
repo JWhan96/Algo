4df9b62af8d616fee00e711094f9c14c7491ec15
Hanjinwoo <chococan16@gmail.com>
2024-03-29 00:53:08 +0900

    24_03_29

--- dd.cpp
+++ dd.cpp
@@ -1,55 +1,41 @@
-#include <algorithm>
-#include <cstring>
 #include <iostream>
-#include <queue>
-#include <vector>
+#include <string>
+
 using namespace std;
 
-int n, m;
-char arr[101][101];
-int dr[] = {-1, 0, 1, 0};
-int dc[] = {0, 1, 0, -1};
-int visit[101][101];
-int cnt = 1;
 int main() {
-  cin >> n >> m;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      cin >> arr[i][j];
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  string a, bomb;
+  string st = "";
+  cin >> a >> bomb;
+  int a_len = a.size();
+  int bomb_len = bomb.size();
+  int st_len = st.size();
+
+  for (int i = 0; i < a.size(); i++) {
+    st += a[i];  // 문자 추가
+    // 임시 문자 길이가 폭발 문자열 보다 크거나 같을 때
+    if (st.size() >= bomb.size()) {
+      // cout << st << endl;
+      bool flag = true;  // 폭발 문자열 있는지 확인하는 flag
+      for (int j = 0; j < bomb.size(); j++) {
+        if (st[st.size() - bomb.size() + j] != bomb[j]) {
+          flag = false;
+          break;
+        }  // t뒤에서 폭발 문자열 길이만큼 비교
+      }
+
+      if (flag)  // 폭발 문자열일 경우 삭제
+                 // st.erase(st.end() - bomb.size(), st.end());
+        for (int i = 0; i < bomb.size(); i++) {
+          st.pop_back();
+        }
     }
   }
-
-  int row = 0;
-  int col = 0;
-  visit[row][col] = 1;
-  queue<pair<int, int>> q;
-  q.push(make_pair(row, col));
-  while (!q.empty()) {
-    row = q.front().first;
-    col = q.front().second;
-    q.pop();
-    for (int i = 0; i < 4; i++) {
-      int nr = row + dr[i];
-      int nc = col + dc[i];
-      if (nr < 0 || nc < 0 || nr >= n || nc >= m) {
-        continue;
-      }
-      if (visit[nr][nc] != 0) {
-        continue;
-      }
-      if (arr[nr][nc] == '0') {
-        continue;
-      }
-      if (arr[nr][nc] == '1') {
-        q.push(make_pair(nr, nc));
-        visit[nr][nc] = visit[row][col] + 1;
-      }
-      if (nr == n - 1 && nc == m - 1) {
-        cout << visit[nr][nc];
-        // return 0;
-      }
-    }
-  }
-
+  if (st.empty())  // 남아 있는 문자열이 없는 경우
+    cout << "FRULA" << '\n';
+  else
+    cout << st << '\n';
   return 0;
 }
