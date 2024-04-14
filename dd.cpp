b69288dda9f90784cb4449ec9900c51b6fa2c52e
Hanjinwoo <chococan16@gmail.com>
2024-04-14 22:28:40 +0900

    24_04_14

--- dd.cpp
+++ dd.cpp
@@ -1,34 +1,21 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-struct p {
-  int age;
-  string name;
-};
-vector<p> arr;
-int n;
-bool compare(const p &a, const p &b) {
-  return a.age < b.age;  // 나이만을 비교
-}
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+int main()
+{
+    int num[6];
+    int sum = 0;
 
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    int a;
-    string b;
-    cin >> a >> b;
-    arr.push_back({a, b});
-  }
-  stable_sort(arr.begin(), arr.end(), compare);
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> num[i];
+        sum += num[i];
+    }
+    sort(num, num+5);
 
-  for (int i = 0; i < n; i++) {
-    cout << arr[i].age << " " << arr[i].name << '\n';
-  }
+    cout << sum / 5 << '\n' << num[2] << '\n';
 
-  return 0;
+    return 0;
 }
