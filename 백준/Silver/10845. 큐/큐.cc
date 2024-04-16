885c1ca5eb3593686fcbcf35543239fc0101389a
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-04-16 09:34:58 +0900

    [Silver IV] Title: 큐, Time: 348 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+string a;
+int n;
+int num;
+queue<int> q;
+int result;
+int main() {
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> a;
+    if (a == "push") {
+      cin >> num;
+      q.push(num);
+    } else if (a == "pop") {
+      if (q.empty()) {
+        cout << -1 << '\n';
+      } else {
+        cout << q.front() << '\n';
+        q.pop();
+      }
+    } else if (a == "front") {
+      if (q.empty()) {
+        cout << -1 << '\n';
+      } else {
+        cout << q.front() << '\n';
+      }
+    } else if (a == "back") {
+      if (q.empty()) {
+        cout << -1 << '\n';
+      } else {
+        cout << q.back() << '\n';
+      }
+    } else if (a == "empty") {
+      if (q.empty()) {
+        cout << 1 << '\n';
+      } else {
+        cout << 0 << '\n';
+      }
+    } else if (a == "size") {
+      cout << q.size() << '\n';
+    }
+  }
+
+  return 0;
+}
