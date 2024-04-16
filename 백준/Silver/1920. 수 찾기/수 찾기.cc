721390ca4670c4540c9af6089357d42d78859766
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-04-17 00:21:56 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[100001];
+int brr[100001];
+int start, endd, mid;
+bool flag;
+int max_arr;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+    max_arr = max(arr[i], max_arr);
+  }
+
+  cin >> m;
+  for (int j = 0; j < m; j++) {
+    cin >> brr[j];
+  }
+  sort(arr, arr + n);
+
+  for (int i = 0; i < m; i++) {
+    if (brr[i] > max_arr) {
+      cout << 0 << '\n';
+      continue;
+    }
+    start = 0;
+    endd = n - 1;
+    flag = 0;
+    while (start <= endd) {
+      flag = 0;
+      mid = (start + endd) / 2;
+      if (arr[mid] == brr[i]) {
+        cout << 1 << '\n';
+        flag = 1;
+        break;
+      } else if (arr[mid] > brr[i]) {
+        endd = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    if (!flag) {
+      cout << 0 << "\n";
+    }
+  }
+  return 0;
+}
