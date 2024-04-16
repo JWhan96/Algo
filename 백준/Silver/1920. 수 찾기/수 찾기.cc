cad7489c73995c7e178e3ce49e2c62cf9c2d7128
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-04-17 00:26:17 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -3,49 +3,45 @@
 
 using namespace std;
 
-int n, m;
+int N, M;
 int arr[100001];
-int brr[100001];
-int start, endd, mid;
-bool flag;
-int max_arr;
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
 
-  cin >> n;
-  for (int i = 0; i < n; i++) {
+void binarySearch(int key) {
+  int start = 0;
+  int end = N - 1;
+  int mid;
+
+  while (end >= start) {
+    mid = (start + end) / 2;
+    if (arr[mid] == key) {
+      cout << 1 << "\n";
+      return;
+    } else if (arr[mid] > key) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  cout << 0 << "\n";
+  return;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> N;
+  int temp;
+
+  for (int i = 0; i < N; i++) {
     cin >> arr[i];
-    max_arr = max(arr[i], max_arr);
   }
 
-  sort(arr, arr + n);
-  cin >> m;
-  for (int j = 0; j < m; j++) {
-    cin >> brr[j];
-    if (brr[j] > max_arr) {
-      cout << 0 << '\n';
-      continue;
-    }
-    start = 0;
-    endd = n - 1;
-    flag = 0;
-    while (start <= endd) {
-      flag = 0;
-      mid = (start + endd) / 2;
-      if (arr[mid] == brr[j]) {
-        cout << 1 << '\n';
-        flag = 1;
-        break;
-      } else if (arr[mid] > brr[j]) {
-        endd = mid - 1;
-      } else {
-        start = mid + 1;
-      }
-    }
-    if (!flag) {
-      cout << 0 << "\n";
-    }
+  sort(arr, arr + N);
+
+  cin >> M;
+  for (int i = 0; i < M; i++) {
+    cin >> temp;
+    binarySearch(temp);
   }
 
   return 0;
