ffd81d6e924d7ce0ed49d53cacc5f62ab1a96a2c
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-04-25 23:32:54 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int n;
+int cnt;
+int main() {
+  cin >> n;
+  for (int i = 5; i <= n; i *= 5) {
+    cnt += n / i;
+  }
+  cout << cnt;
+  return 0;
+}
