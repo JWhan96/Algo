2695182a29e0a94584b775fe0844969ebcba59e2
jinwoo <chococan16@gmail.com>
2024-07-04 00:05:59 +0900

    24-07-04

--- dd.cpp
+++ dd.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <queue>
 
@@ -7,43 +8,76 @@ struct pose {
   int row;
   int col;
 };
-int arr[9][9];
-int visit[9][9];
+int N;
+int arr[101][101];
+int record[101][101];
+int maxT = 0;
+int area = 1;
 int cnt;
-int n, m;
-int dr[] = {0, 1, 0, -1);
+int dr[] = {0, 1, 0, -1};
 int dc[] = {1, 0, -1, 0};
 queue<pose> q;
 
-void bfs() {
+void bfs(int row, int col) {
+  record[row][col] = 1;
+  q.push({row, col});
   while (!q.empty()) {
     int row = q.front().row;
     int col = q.front().col;
+    q.pop();
+    for (int i = 0; i < 4; i++) {
+      int nr = row + dr[i];
+      int nc = col + dc[i];
+      if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
+        continue;
+      }
+      if (record[nr][nc] != 0) {
+        continue;
+      }
+      q.push({nr, nc});
+      record[nr][nc] = 1;
+    }
   }
-  return;
 }
 
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      cin >> arr[i][j];
-      if (arr[i][j] == 0) {
-        cnt++;
-      }
-      if (arr[i][j] == 2) {
-        q.push({i, j});
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int a;
+      cin >> a;
+      arr[i][j] = a;
+      if (maxT < a) {
+        maxT = a;
       }
     }
   }
-  // 조합();
-  int start_row = q.front().row;
-  int start_col = q.front().col;
-  visit[start_row][start_col] = 1;
-  bfs();
-  cout << cnt;
+  for (int k = 1; k <= maxT; k++) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (arr[i][j] <= k) {
+          record[i][j] = 1;
+        } else {
+          record[i][j] = 0;
+        }
+      }
+    }
+
+    // bfs돌리면서 영역 찾기
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (record[i][j] == 0) {
+          bfs(i, j);
+          cnt += 1;
+        }
+      }
+    }
+    if (area < cnt) {
+      area = cnt;
+    }
+    cnt = 0;
+  }
+  cout << area;
+
   return 0;
 }
