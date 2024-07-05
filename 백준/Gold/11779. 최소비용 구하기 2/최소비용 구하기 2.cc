3ccddb8403091318bce65c1df61bcffeeb786c2b
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-07-05 14:57:18 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 28 ms, Memory: 4752 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -10,7 +10,6 @@ struct Edge {
   int to;
   bool operator<(Edge right) const { return cost > right.cost; }
 };
-
 int cntNode, cntEdge;
 int startBus, endBus;
 vector<int> stopv;
@@ -22,7 +21,7 @@ int from[1003];
 void dijkstra(int st) {
   for (int i = 1; i <= cntNode; i++) {
     dist[i] = 1e9;
-    from[i] = -1;  // Initialize from array to -1
+    // from[i] = -1;
   }
   pq.push({0, st});
   dist[st] = 0;
@@ -39,28 +38,18 @@ void dijkstra(int st) {
       int next = nextEdge.to;
       int nextCost = nextEdge.cost;
 
-      if (dist[next] > dist[now] + nextCost) {
-        dist[next] = dist[now] + nextCost;
-        from[next] = now;
-        pq.push({dist[next], next});
-      }
+      if (dist[next] <= dist[now] + nextCost) continue;
+      // stopv.push_back(next);
+      dist[next] = dist[now] + nextCost;
+      from[next] = now;
+      pq.push({dist[next], next});
     }
   }
-}
-
-void getPath(int end) {
-  vector<int> path;
-  for (int at = end; at != -1; at = from[at]) {
-    path.push_back(at);
-  }
-  reverse(path.begin(), path.end());
-  stopv = path;
 }
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
   cin >> cntNode >> cntEdge;
   for (int i = 0; i < cntEdge; i++) {
     int from, to, cost;
@@ -68,14 +57,28 @@ int main() {
     v[from].push_back({cost, to});
   }
   cin >> startBus >> endBus;
-
   dijkstra(startBus);
-  getPath(endBus);
 
-  cout << dist[endBus] << "\n";  // 최소 비용 출력
-  cout << stopv.size() << "\n";  // 경로 길이 출력
+  // for (int at = endBus; at != -1; at = from[at]) {
+  //   stopv.push_back(at);
+  // }
+  // reverse(stopv.begin(), stopv.end());
+
+  int at = endBus;
+  while (at != 0) {
+    stopv.push_back(at);
+    at = from[at];
+  }
+  reverse(stopv.begin(), stopv.end());
+
+  cout << dist[endBus] << "\n";
+  // 어떤벡터의 사이즈
+  // 그 벡터 순서대로 출력
+  // 그럼 언제 이 벡터에 추가를 하면 좋을 것인가
+  cout << stopv.size() << "\n";
   for (int i = 0; i < stopv.size(); i++) {
     cout << stopv[i] << " ";  // 경로 출력
   }
+
   return 0;
 }
