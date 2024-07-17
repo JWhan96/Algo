3848eea76e94d697344c25482915a0dbb145eb87
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-07-17 21:25:01 +0900

    [Silver II] Title: 나무 자르기, Time: 172 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -5,43 +5,45 @@ using namespace std;
 
 long long N, result;
 vector<long long> v;
+
 long long maxTree;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    cin >> N >> result;
-    for (int i = 0; i < N; i++) {
-        long long a;
-        cin >> a;
-        v.push_back(a);
-        if (a > maxTree) {
-            maxTree = a;
-        }
+  cin >> N >> result;
+  for (int i = 0; i < N; i++) {
+    long long a;
+    cin >> a;
+    v.push_back(a);
+    if (a > maxTree) {
+      maxTree = a;
     }
+  }
+  long long startP = 0;
+  long long endP = maxTree;
+  long long point = 0;
+  while (startP <= endP) {
+    long long num;
+    num = 0;
+    point = (endP + startP) / 2;
 
-    long long startP = 0;
-    long long endP = maxTree;
-    long long point = 0;
-
-    while (startP <= endP) {
-        point = (startP + endP) / 2;
-        long long num = 0;
-
-        for (int i = 0; i < v.size(); i++) {
-            if (v[i] > point) {
-                num += (v[i] - point);
-            }
-        }
-
-        if (num < result) {
-            endP = point - 1;
-        } else {
-            startP = point + 1;
-        }
+    for (int i = 0; i < v.size(); i++) {
+      long long cut = v[i] - point;
+      if (cut <= 0) continue;
+      num += cut;
     }
+    if (num < result)
+      endP = point - 1;
+    else
+      startP = point + 1;
 
-    cout << endP << endl;
-    return 0;
+    if (num == result) {
+      cout << point << endl;
+      return 0;
+    }
+  }
+  cout << endP << endl;
+  return 0;
 }
