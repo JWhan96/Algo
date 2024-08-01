10e43928742b3dd6993e8d214d38d0bf01db40e1
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-08-01 22:13:16 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,66 +1,46 @@
-#include <algorithm>
-#include <climits>
 #include <iostream>
-#include <vector>
-#define endl '\n'
 using namespace std;
 
 int N;
-vector<int> numArr;
-
-int Operate(int a, int b, char op) {
-  switch (op) {
-    case '+':
-      return a + b;
-    case '-':
-      return a - b;
-    case '*':
-      return a * b;
-    case '/':
-      return a / b;
-    default:
-      return 0;
-  }
+int operands[11]; // 수열 
+int operators[4]; // 연산자의 개수
+int mymin = 1000000001;
+int mymax = -1000000001;
+void getanswer(int result, int idx)
+{
+    if(idx == N)
+    {
+        if(result > mymax)
+            mymax = result;
+        if(result < mymin)
+            mymin = result;
+        return;
+    }
+    for(int i = 0; i < 4; i++)
+    {
+        if(operators[i] > 0)
+        {
+            operators[i]--; // 연산자 하나 사용했으므로 1개 줄여줌
+            if(i == 0)
+                getanswer(result + operands[idx], idx+1);
+            else if(i == 1)
+                getanswer(result - operands[idx], idx+1);
+            else if(i == 2)
+                getanswer(result * operands[idx], idx+1);
+            else
+                getanswer(result / operands[idx], idx+1);
+            operators[i]++; // 다른 연산자를 사용할 것이므로 아까 줄였던 연산자 개수 늘려줌
+        }
+    }
+    return;
 }
-
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  cin >> N;
-  for (int n = 0; n < N; n++) {
-    int num;
-    cin >> num;
-    numArr.push_back(num);
-  }
-  int add, sub, mul, div;
-  cin >> add >> sub >> mul >> div;
-  vector<char> oper;
-  oper.insert(oper.end(), add, '+');
-  oper.insert(oper.end(), sub, '-');
-  oper.insert(oper.end(), mul, '*');
-  oper.insert(oper.end(), div, '/');
-  sort(oper.begin(), oper.end());
-  int i = 0;
-  int minValue = INT_MAX;
-  int maxValue = INT_MIN;
-  do {
-    int result = numArr[0];
-    for (int i = 1; i < N; i++) {
-      result = Operate(result, numArr[i], oper[i - 1]);  // *+ +*
-      // cout << i << ": " << result << " " << endl;
-    }
-
-    if (result < minValue) {
-      minValue = result;
-    }
-    if (result > maxValue) {
-      maxValue = result;
-    }
-
-  } while (next_permutation(oper.begin(), oper.end()));
-
-  cout << maxValue << endl;
-  cout << minValue << endl;
-  return 0;
+    cin >> N;
+    for(int i = 0; i < N; i++)
+        cin >> operands[i];
+    for(int i = 0; i < 4; i++)
+        cin >> operators[i];
+    getanswer(operands[0],1);
+    cout << mymax << '\n';
+    cout << mymin;
 }
