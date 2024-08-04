9eae04d6c0706d2b142d09dd767a5f1e777653e7
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-08-05 03:21:04 +0900

    [Gold II] Title: 네트워크 복구, Time: 56 ms, Memory: 6696 KB -BaekjoonHub

--- 백준/Gold/2211. 네트워크 복구/네트워크 복구.cc
+++ 백준/Gold/2211. 네트워크 복구/네트워크 복구.cc
@@ -1,75 +1,68 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <set>
+#include <vector>
+
+#define endl '\n'
+#define fastio             \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0);
 
 using namespace std;
 
-typedef pair<int, int> pii;
-const int MAX = 1001, INF = 987654321;
+struct Node {
+  int cost;
+  int to;
+  bool operator<(Node right) const { return cost > right.cost; }
+};
+int cntNode, cntEdge;
+vector<Node> v[1005];
+int dist[1005];
+priority_queue<Node> pq;
+vector<pair<int, int>> node;
+int Parent[1005];
 
-int N, M, A, B, C;
-vector<pii> adj[MAX];
+int main() {
+  fastio;
+  cin >> cntNode >> cntEdge;
+  for (int i = 0; i < cntEdge; i++) {
+    int from, to, cost;
+    cin >> from >> to >> cost;
+    v[from].push_back({cost, to});
+    v[to].push_back({cost, from});
+  }
+  for (int i = 0; i < 1005; i++) {
+    dist[i] = 1e9;
+  }
+  dist[1] = 0;
+  pq.push({0, 1});
+  while (!pq.empty()) {
+    Node nowNode = pq.top();
+    pq.pop();
+    int nowCost = nowNode.cost;
+    int now = nowNode.to;
+    if (nowCost > dist[now]) continue;
 
-set<pii> dijkstra() {
-    set<pii> ret;
-    vector<int> d(N+1, INF);
-    vector<int> before(N+1);
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    
-    for (int i=0; i<=N; i++) before[i] = i;
-    d[1] = 0;
-    pq.push({0, 1});
-    
-    while (!pq.empty()) {
-        int currD = pq.top().first;
-        int curr = pq.top().second;
-        pq.pop();
-        
-        if (currD > d[curr]) continue;
-        
-        for (auto x: adj[curr]) {
-            int next = x.first;
-            int w = x.second;
-            
-            if (d[curr]+w < d[next]) {
-                d[next] = d[curr]+w;
-                before[next] = curr;
-                pq.push({d[next], next});
-            }
-        }
+    for (int i = 0; i < v[now].size(); i++) {
+      int next = v[now][i].to;
+      int nextCost = v[now][i].cost;
+
+      if (dist[next] <= dist[now] + nextCost) continue;
+
+      dist[next] = dist[now] + nextCost;
+      pq.push({dist[next], next});
+      Parent[next] = now;  // 부모 노드 저장
     }
+  }
 
-    for (int i=1; i<=N; i++) {
-        int p = i;
-        
-        while (p != before[p]) {
-            ret.insert({min(p, before[p]), max(p, before[p])});  
-            p = before[p];
-        }
-    }
-    
-    return ret;
-}
+  // 복구된 경로의 개수 출력
+  cout << cntNode - 1 << endl;
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
-    cin >> N >> M;
-    for (int i=0; i<M; i++) {
-        cin >> A >> B >> C;
-        adj[A].push_back({B, C});
-        adj[B].push_back({A, C});
-    }
-    
-    set<pii> ans = dijkstra();
-    
-    cout << ans.size() << "\n";
-    for (auto edge: ans) {
-        cout << edge.first << " " << edge.second << "\n";
-    }
+  // 각 복구된 경로의 상세 정보 출력
+  for (int i = 2; i <= cntNode; i++) {
+    cout << Parent[i] << " " << i << endl;
+  }
 
-    return 0;
+  return 0;
 }
