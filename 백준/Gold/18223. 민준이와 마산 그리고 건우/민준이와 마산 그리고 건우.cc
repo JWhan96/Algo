6454e8456d2639ee8dac9a686800009ddc60b324
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-08-12 17:39:05 +0900

    [Gold IV] Title: 민준이와 마산 그리고 건우, Time: 4 ms, Memory: 2684 KB -BaekjoonHub

--- 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.cc
+++ 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.cc
@@ -1,80 +1,66 @@
+#include <algorithm>
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <algorithm>
 
 #define endl '\n'
-#define INF 1e9
+#define fastio             \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0);
 
 using namespace std;
-
 struct Node {
-    int cost;
-    int to;
-    bool operator<(Node right) const { return cost > right.cost; }
+  int cost;
+  int to;
+  bool operator<(Node right) const { return cost > right.cost; }
 };
+int cntNode, cntEdge, goal;
+vector<Node> v[5005];
 
-int cntNode, cntEdge, p;
-vector<Node> adj[5005];
-
-vector<int> dijkstra(int start) {
-    vector<int> dist(cntNode + 1, INF);
-    priority_queue<Node> pq;
-    dist[start] = 0;
-    pq.push({0, start});
-
-    while (!pq.empty()) {
-        int now = pq.top().to;
-        int nowCost = pq.top().cost;
-        pq.pop();
-
-        if (nowCost > dist[now]) continue;
-
-        for (auto nextNode : adj[now]) {
-            int next = nextNode.to;
-            int nextCost = nextNode.cost;
-
-            if (dist[next] > dist[now] + nextCost) {
-                dist[next] = dist[now] + nextCost;
-                pq.push({dist[next], next});
-            }
-        }
+vector<int> daikstra(int start) {
+  vector<int> dist(cntNode + 1, 1e9);
+  priority_queue<Node> pq;
+  // for (int i = 0; i <= cntNode; i++) {
+  //   dist[i] = 1e9;
+  // }
+  pq.push({0, start});
+  dist[start] = 0;
+  while (!pq.empty()) {
+    Node nowNode = pq.top();
+    pq.pop();
+    int now = nowNode.to;
+    int nowCost = nowNode.cost;
+    if (nowCost > dist[now]) continue;
+    for (int i = 0; i < v[now].size(); i++) {
+      Node nextNode = v[now][i];
+      int next = nextNode.to;
+      int nextCost = nextNode.cost;
+      if (dist[next] <= dist[now] + nextCost) continue;
+      dist[next] = dist[now] + nextCost;
+      pq.push({dist[next], next});
     }
-
-    return dist;
+  }
+  return dist;
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> cntNode >> cntEdge >> p;
-    
-    for (int i = 0; i < cntEdge; i++) {
-        int from, to, cost;
-        cin >> from >> to >> cost;
-        adj[from].push_back({cost, to});
-        adj[to].push_back({cost, from});
-    }
-
-    // 1. 1번 노드에서 모든 노드까지의 최단 경로 계산
-    vector<int> distFrom1 = dijkstra(1);
-
-    // 2. p번 노드에서 모든 노드까지의 최단 경로 계산
-    vector<int> distFromP = dijkstra(p);
-
-    // 3. 1번 노드에서 p번 노드를 거쳐 마지막 노드로 가는 경로의 비용
-    int totalDistViaP = distFrom1[p] + distFromP[cntNode];
-
-    // 4. 1번 노드에서 마지막 노드로 바로 가는 경로의 비용
-    int directDist = distFrom1[cntNode];
-
-    // 5. 경로 비교
-    if (totalDistViaP == directDist) {
-        cout << "SAVE HIM" << endl;
-    } else {
-        cout << "GOOD BYE" << endl;
-    }
-
-    return 0;
+  fastio;
+  cin >> cntNode >> cntEdge >> goal;
+  for (int i = 0; i < cntEdge; i++) {
+    int from, to, cost;
+    cin >> from >> to >> cost;
+    v[from].push_back({cost, to});
+    v[to].push_back({cost, from});
+  }
+  vector<int> daik1 = daikstra(1);
+  vector<int> daikGoal = daikstra(goal);
+  int basicCost = daik1[cntNode];
+  int totalCost = daikGoal[1] + daikGoal[cntNode];
+  if (basicCost == totalCost) {
+    cout << "SAVE HIM" << endl;
+  } else {
+    cout << "GOOD BYE" << endl;
+  }
+  return 0;
 }
