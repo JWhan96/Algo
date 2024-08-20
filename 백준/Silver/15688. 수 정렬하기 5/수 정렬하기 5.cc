0b7935f5fdf2c8f523c1ac883eaf50db95da5e50
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-08-20 18:54:49 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4036 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#define fastio             \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0);
+#define endl '\n'
+
+using namespace std;
+
+int N;
+int main() {
+  fastio;
+  cin >> N;
+  vector<int> v(N);
+  for (int i = 0; i < N; i++) {
+    cin >> v[i];
+  }
+  sort(v.begin(), v.end());
+  for (int s : v) {
+    cout << s << endl;
+  }
+}
