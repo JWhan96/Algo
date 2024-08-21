082ee3b94741a5ef97b52abdba2a182d868817c5
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-08-21 15:19:23 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,44 +1,24 @@
-#include <algorithm>
 #include <iostream>
-#include <list>
-#include <string>
-#include <vector>
-#define fastio             \
-  ios::sync_with_stdio(0); \
-  cin.tie(0);              \
-  cout.tie(0);
-#define endl '\n'
+#include <queue>
 
 using namespace std;
 
-int prv[5005];
-int nxt[5005];
-
-int main() {
-  fastio;
-  int N, M;
-  cin >> N >> M;
-  vector<int> v;
-  int len = N;
-  for (int i = 1; i <= N; i++) {
-    prv[i] = (i == 1) ? N : i - 1;
-    nxt[i] = (i == N) ? 1 : i + 1;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, k;
+  cin >> n >> k;
+  queue<int> Q;
+  for (int i = 1; i <= n; i++) Q.push(i);
+  cout << '<';
+  while (!Q.empty()) {
+    for (int i = 0; i < k - 1; i++) {
+      Q.push(Q.front());
+      Q.pop();
+    }
+    cout << Q.front();
+    Q.pop();
+    if (Q.size()) cout << ", ";
   }
-  int i = 1;
-  for (int cur = 1; len != 0; cur = nxt[cur]) {
-    if (i == M) {
-      nxt[prv[cur]] = nxt[cur];
-      prv[nxt[cur]] = prv[cur];
-      v.push_back(cur);
-      i = 1;
-      len--;
-    } else
-      i++;
-  }
-  cout << "<";
-  for (size_t i = 0; i < v.size(); ++i) {
-    cout << v[i];
-    if (i != v.size() - 1) cout << ", ";
-  }
-  cout << ">";
+  cout << '>';
 }
