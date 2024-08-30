2e6136aa1c2ef8a4104c92202bed41f6094e7b63
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-08-30 15:50:26 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -9,17 +9,18 @@
 #define endl '\n'
 
 using namespace std;
+typedef long long ll;
 
 int N;
-pair<long long, long long> dp[100];
+pair<ll, ll> dp[100];
 int main() {
   fastio;
   cin >> N;
   dp[1] = {0, 1};
   dp[2] = {1, 0};
   for (int i = 3; i <= N; i++) {
-    long long c0 = dp[i - 1].first + dp[i - 1].second;
-    long long c1 = dp[i - 2].second + dp[i - 2].first;
+    ll c0 = dp[i - 1].first + dp[i - 1].second;
+    ll c1 = dp[i - 2].second + dp[i - 2].first;
     dp[i] = {c0, c1};
   }
   cout << dp[N].first + dp[N].second;
