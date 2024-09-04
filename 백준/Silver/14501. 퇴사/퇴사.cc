e1f9c38855785e87254a7af0b66b3147be2aa1b6
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-09-04 13:05:26 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
-#include <vector>
+
 #define fastio             \
   ios::sync_with_stdio(0); \
   cin.tie(0);              \
@@ -10,50 +9,30 @@
 
 using namespace std;
 
+int dp[30] = {};
+
+int day[20];
+int pay[20];
 int N;
-int dp[17][17];
+
 int main() {
   fastio;
   cin >> N;
-  vector<pair<int, int>> v(N + 1);
+
   for (int i = 1; i <= N; i++) {
     int d, p;
-    cin >> d >> p;
-    v[i] = {d, p};
+    // cin >> d >> p;
+    cin >> day[i] >> pay[i];
   }
 
-  int max_R = 0;
-  for (int i = 1; i <= N; i++) {
-    int max_C = 0;
-    for (int k = 0; k < i; k++) {
-      max_C = max(dp[k][i], max_C);
-      // cout << "dp " << dp[k][i] << " " << "mc " << max_C << " ";
-    }
-    // cout << endl;
-    // cout << max_C << endl;
-    for (int j = 0; j <= N; j++) {
-      int day = v[i].first;
-      int pay = v[i].second;
-      if (j > 0 && j < i) continue;
-      if (j == 0) {
-        dp[i][j] = max_R;
-        continue;
-      }
+  for (int i = 1; i <= N + 1; i++) {
+    dp[i] = max(dp[i], dp[i - 1]);  // 이전날짜를 했을 경우와 안했을 경우 max
 
-      if (j == i) {
-        if (i + day - 1 > N) break;
-        dp[i][j] = max_C + pay;
-        max_R = max(max_R, dp[i][j]);
-        j += day - 1;
-        continue;
-      }
-      if (j > 0) {
-        // if (j + day - 1 > N) continue;
-        dp[i][j] = max_C + pay;
-        max_R = max(max_R, dp[i][j]);
-      }
-    }
+    dp[i + day[i]] =
+        max(dp[i + day[i]],
+            dp[i] + pay[i]);  // day만큼 일 못하므로 그 다음날의 최대 수익 계산
   }
 
-  cout << max_R;
+  cout << dp[N + 1];
+  return 0;
 }
