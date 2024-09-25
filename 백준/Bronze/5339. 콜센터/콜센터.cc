f669f29f67fb0051f5c06985a44df22497e138e2
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2024-09-25 23:07:53 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#define endl '\n'
+#define fastio             \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0);
+
+using namespace std;
+
+int main() {
+  fastio;
+  cout << "     /~\\" << endl;
+  cout << "    ( oo|" << endl;
+  cout << "    _\\=/_" << endl;
+  cout << "   /  _  \\" << endl;
+  cout << "  //|/.\\|\\\\" << endl;
+  cout << " ||  \\ /  ||" << endl;
+  cout << "============" << endl;
+  cout << "|          |" << endl;
+  cout << "|          |" << endl;
+  cout << "|          |" << endl;
+}
