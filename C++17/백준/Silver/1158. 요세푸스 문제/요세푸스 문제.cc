fa9a196fa0a588cf819cf584d4653a93edffd5ce
JWhan96 <139518217+JWhan96@users.noreply.github.com>
2026-04-11 18:03:50 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 92 ms, Memory: 2284 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int N, K;
+  cin >> N >> K;
+  list<int> l;
+  for (int i = 1; i <= N; i++) {
+    l.push_back(i);
+  }
+  // 첫 시작
+  auto cur = l.begin();
+  // list가 빌때까지
+  cout << '<';
+  while (l.size() > 0) {
+    for (int i = 0; i < K - 1; i++) {
+      if (cur == l.end()) {
+        cur = l.begin();
+      }
+      cur++;
+      if (cur == l.end()) {
+        cur = l.begin();
+      }
+    }
+
+    int a = *cur;
+
+    cur = l.erase(cur);
+    if (l.size() != 0) {
+      cout << a << ", ";
+
+    } else {
+      cout << a;
+    }
+  }
+  cout << '>';
+
+  return 0;
+}
