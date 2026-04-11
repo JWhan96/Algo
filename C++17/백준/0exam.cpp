fadead0fffbdde28516f1516f875ebebe16fd690
JWhan96 <chococan16@gmail.com>
2026-04-11 18:08:22 +0900

    2026-04-11

--- C++17/백준/0exam.cpp
+++ C++17/백준/0exam.cpp
@@ -1,39 +1,43 @@
 #include <bits/stdc++.h>
 
-#include <algorithm>
 using namespace std;
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  // N: 학생수, K :한방 최대 인원, 0여, 1남
+
   int N, K;
   cin >> N >> K;
-
-  // 성별, 학년
-  int arr[2][7] = {};
-  for (int i = 0; i < N; i++) {
-    int sex, grade;
-    cin >> sex >> grade;
-    arr[sex][grade]++;
+  list<int> l;
+  for (int i = 1; i <= N; i++) {
+    l.push_back(i);
   }
-
-  int cnt = 0;
-  for (int i = 1; i < 7; i++) {
-    if ((arr[0][i] % K) == 0) {
-      cnt += arr[0][i] / K;
-    } else if ((arr[0][i] % K) != 0) {
-      cnt += (arr[0][i] / K) + 1;
+  // 첫 시작
+  auto cur = l.begin();
+  // list가 빌때까지
+  cout << '<';
+  while (!l.empty()) {
+    for (int i = 0; i < K - 1; i++) {
+      if (cur == l.end()) {
+        cur = l.begin();
+      }
+      cur++;
+      if (cur == l.end()) {
+        cur = l.begin();
+      }
     }
 
-    if ((arr[1][i] % K) == 0) {
-      cnt += (arr[1][i] / K);
-    } else if ((arr[1][i] % K) != 0) {
-      cnt += (arr[1][i] / K) + 1;
+    int a = *cur;
+
+    cur = l.erase(cur);
+    if (l.size() != 0) {
+      cout << a << ", ";
+
+    } else {
+      cout << a;
     }
   }
-
-  cout << cnt;
+  cout << '>';
 
   return 0;
 }
